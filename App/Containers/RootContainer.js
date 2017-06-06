7b6f6f6802a201e8d113b48456940a3e34759cd6
Sam Wang <sam.zz.wang@gmail.com>
2017-06-05 21:22:04 -0700

    Add App and RootContainer

---
+++ App/Containers/RootContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class RootContainer extends Component {
+  render () {
+    return(
+      <View>
+        <Text>See n Me</Text>
+      </View>
+    );
+  }
+}
+
+export default RootContainer;
