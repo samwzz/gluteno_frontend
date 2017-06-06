2b936685afbb96efacd97f08d791596e0f5f7d37
Vu Pham <vutpham@outlook.com>
2017-06-05 23:14:27 -0700

    First commit:  Signup

---
+++ App/Containers/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-redux';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <View>
+        <
+      </View>
+    );
+  }
+}
+
+export default Login;
