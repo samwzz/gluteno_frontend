aa4439c2f42a4d065e3314c576a9d857657f7d49
Vu Pham <vutpham@outlook.com>
2017-06-07 10:13:21 -0700

    Comment out autofill search

--- App/Containers/RestaurantAdd.js
+++ App/Containers/RestaurantAdd.js
@@ -6,14 +6,15 @@ import { View,
   AsyncStorage,
   StyleSheet
 } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 class RestaurantAdd extends Component {
   constructor() {
     super();
     this.state = {
-      name = "",
-      place_id = ""
-    }
+      restaurant = '',
+      place_id = ''
+    };
   }
 
   onRestaurantAdd() {
@@ -23,19 +24,72 @@ class RestaurantAdd extends Component {
   render() {
     <View>
       <Text style={styles.heading}>
-        SignUp for See-n-Me
+        See-n-Me Logo Here
       </Text>
 
-      <TextInput
-        onChangeText={ (text)=> this.setState({fname: text}) }
-        style={styles.input} placeholder="Restaurant">
-      </TextInput>
+      {/* Here we want AutoComplete TextFill
+
+      <GooglePlacesAutocomplete
+        placeholder='Search'
+        minLength={2} // minimum length of text to search
+        autoFocus={false}
+        returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+        listViewDisplayed='auto'    // true/false/undefined
+        fetchDetails={true}
+        renderDescription={(row) => row.description} // custom description render
+        onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+          console.log(data);
+          console.log(details);
+        }}
+        getDefaultValue={() => {
+          return ''; // text input default value
+        }}
+        query={{
+          // available options: https://developers.google.com/places/web-service/autocomplete
+          key: 'AIzaSyBDwGj9-ZQ1ID90JRABgmbmQl0AJCnrU70',
+          language: 'en', // language of the results
+          types: '(cities)', // default: 'geocode'
+        }}
+        styles={{
+          description: {
+            fontWeight: 'bold',
+          },
+          predefinedPlacesDescription: {
+            color: '#1faadb',
+          },
+        }}
+
+        currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+        currentLocationLabel="Current location"
+        nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+        GoogleReverseGeocodingQuery={{
+          // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+        }}
+        GooglePlacesSearchQuery={{
+          // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+          rankby: 'distance',
+          types: 'food',
+        }}
+
+
+        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+
+        predefinedPlaces={[homePlace, workPlace]}
+
+        debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
+        renderLeftButton={() => <Image source={require('path/custom/left-icon')} />}
+        renderRightButton={() => <Text>Custom text after the inputg</Text>}
+      />
+
+      */}
 
       <TouchableHighlight onPress={this.onRestaurantAdd.bind(this)} style={styles.button}>
         <Text style={styles.buttonText}>
           Add Restaurant
         </Text>
       </TouchableHighlight>
+
+
 
     </View>
   }
