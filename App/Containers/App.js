98a26e76deebbc3e2d137d7e8da6b82b8d883c77
Vu Pham <vutpham@outlook.com>
2017-06-06 19:52:25 -0700

    Test signup form

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import RootContainer from './RootContainer';
 
 class App extends Component {
--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Header from '../Components/Header';
+import AuthForm from '../Components/AuthForm';
 
 class RootContainer extends Component {
   render () {
     return(
       <View>
         <Header />
+        <AuthForm />
       </View>
     );
   }
