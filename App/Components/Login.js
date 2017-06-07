78f358b8ae51221b312a041cef1483c61365ab3f
Sam Wang <sam.zz.wang@gmail.com>
2017-06-06 19:52:49 -0700

    Login form showing

--- App/Components/Login.js
+++ App/Components/Login.js
@@ -19,6 +19,42 @@ class Login extends Component {
     };
   }
 
+  onLoginPressed() {
+    fetch('https://test.com', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    body: JSON.stringify({
+        session: {
+          email: this.state.email,
+          password: this.state.password,
+        }
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if (responseJson.status >= 200 && responseJson.status < 300) {
+        //Handle success
+        let accessToken = responseJson;
+        console.log(accessToken);
+        //On success we will store the access_token in the AsyncStorage
+        this.storeToken(accessToken);
+       //  this.redirect('home');
+      } else {
+        //Handle error
+        let error = responseJson;
+        throw error;
+      }
+    })
+    .catch((error) => {
+        this.setState({error: error});
+        console.log("error " + error);
+        this.setState({showProgress: false});
+    });
+  }
+
   render() {
     return(
       <View style={styles.container}>
--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Header from '../Components/Header';
+import Login from '../Components/Login';
 
 class RootContainer extends Component {
   render() {
     return(
       <View>
         <Header headerText={"See-n-Me"} />
+        <Login />
       </View>
     );
   }
