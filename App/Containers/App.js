dfb2eeeb2a6ad42e6567e1366762d86045c76a61
Sam Wang <sam.zz.wang@gmail.com>
2017-06-07 09:42:13 -0700

    Add store and reducers

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import RootContainer from './RootContainer';
+import Reducers from '../Reducers';
 
 class App extends Component {
   render() {
     return(
-      <RootContainer />
+      <Provider store={createStore(Reducers)}>
+        <RootContainer />
+      </Provider>
     );
   }
 }
