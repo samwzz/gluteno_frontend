b6fc196337ead71706bf89683bb5a620134fb1c4
Sam Wang <sam.zz.wang@gmail.com>
2017-06-07 22:55:27 -0700

    Change login action

--- app/actions/SessionActions.js
+++ app/actions/SessionActions.js
@@ -1,4 +1,5 @@
 import * as APIUtil from '../util/SessionApiUtil';
+import { AsyncStorage } from 'react-native';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
@@ -21,9 +22,30 @@ export const signup = user => dispatch => (
 
 export const login = user => dispatch => (
   APIUtil.login(user)
-    .then(formUser => dispatch(receiveCurrentUser(formUser)),
-      err => dispatch(receiveErrors(err.responseJSON)))
-);
+    .then(formUser => dispatch(receiveCurrentUser(formUser))
+    .then((responseJson) => {
+      console.log(responseJson);
+    })
+  //     if (responseJson.status >= 200 && responseJson.status < 300) {
+  //       //Handle success
+  //       let accessToken = responseJson;
+  //       console.log(accessToken);
+  //       //On success we will store the access_token in the AsyncStorage
+  //       this.storeToken(accessToken);
+  //      //  this.redirect('home');
+  //     } else {
+  //       //Handle error
+  //       let error = responseJson;
+  //       throw error;
+  //     }
+  //   })
+  //   .catch((error) => {
+  //       this.setState({error: error});
+  //       console.log("error " + error);
+  //       // this.setState({showProgress: false});
+  //   }
+  // ));
+));
 
 export const logout = () => dispatch => (
   APIUtil.logout().then(() => dispatch(receiveCurrentUser(null)))
