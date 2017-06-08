f3f5a1ea8010f4955701b9d8fc430209413283b7
Sam Wang <sam.zz.wang@gmail.com>
2017-06-07 20:56:16 -0700

    Add restaurant reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './SessionReducer';
+import RestaurantReducer from './RestaurantReducer';
 
 const RootReducer = combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  restaurants: RestaurantReducer
 });
 
 export default RootReducer;
