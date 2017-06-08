428e7617d17ddca1a1e4cb307bb6504c43339c6e
Vu Pham <vutpham@outlook.com>
2017-06-08 10:41:12 -0700

    Start on searchbar and restaurant show

---
+++ app/Components/SearchBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { View,
+  Text,
+  FlatList,
+  ListItem,
+  StyleSheet
+} from 'react-native';
+
+import SearchBar from 'react-native-search-bar';
