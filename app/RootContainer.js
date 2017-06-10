9c941ee25e9ff1934d3e051c5e200c194d9d472e
Vu Pham <vutpham@outlook.com>
2017-06-09 21:05:17 -0700

    Fix errors in addRestaurant

--- app/RootContainer.js
+++ app/RootContainer.js
@@ -6,12 +6,13 @@ import Login from './components/Login';
 import RestaurantList from './components/RestaurantList';
 import MapScreen from './screens/MapScreen';
 import { MainNavigator } from './config/router';
+import RestaurantAddScreen from './screens/RestaurantAddScreen';
 
 class RootContainer extends Component {
   render() {
     return(
       <View style={styles.container}>
-        <MainNavigator />
+        <RestaurantAddScreen />
       </View>
     );
   }
--- app/screens/RestaurantAddScreen.js
+++ app/screens/RestaurantAddScreen.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-export default class RestaurantAdd extends Component {
+class RestaurantAddScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      place_id: "",
+      lat: "",
+      lng: ""
+    };
+  }
+
+  onRestaurantAddPress() {
+
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Hi </Text>
+      </View>
+    );
+  }
 
 }
+
+export default RestaurantAddScreen;
