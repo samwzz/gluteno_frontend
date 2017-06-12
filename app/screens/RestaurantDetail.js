031c36f468c3ccfec1aefbdb2df8ab6ecb7ac2df
Vu Pham <vutpham@outlook.com>
2017-06-11 22:21:46 -0700

    Add swipes

--- app/screens/RestaurantDetail.js
+++ app/screens/RestaurantDetail.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight, StyleSheet, TouchableOpacity } from 'react-native';
 import { fetchRestaurantDetails,
          receiveRestaurantDetails,
          receiveRestaurantMap } from '../actions/RestaurantDetailsActions';
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from '../components/common';
+import Swipeable from 'react-native-swipeable';
 
 class RestaurantDetail extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentlyOpenSwipeable: null
+    };
   }
+
 
   componentDidMount() {
     // this.props.fetchRestaurantDetails(this.props.match.params.id);
     // this.props.fetchRestaurantMap(this.props.match.params.id);
   }
 
+  handleScroll() {
+    const {currentlyOpenSwipeable} = this.state;
+
+    if (currentlyOpenSwipeable) {
+      currentlyOpenSwipeable.recenter();
+    }
+  }
+
+
   render() {
+
     // console.log(this.props.navigation.state.params);
     const { name, address, phone_number, lat, lng } = this.props.navigation.state.params;
+
+    const {currentlyOpenSwipeable} = this.state;
 
     return(
       <Card>
@@ -41,11 +59,55 @@ class RestaurantDetail extends Component {
           <Text> Map goes here </Text>
         </CardSection>
 
+        <Swipeable
+        leftContent={(
+          <View style={[styles.leftSwipeItem, {backgroundColor: 'lightskyblue'}]}>
+            <Text>Pull action</Text>
+          </View>
+        )}
+        rightButtons={[
+          <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: 'lightseagreen'}]}>
+            <Text>1</Text>
+          </TouchableOpacity>,
+          <TouchableOpacity style={[styles.rightSwipeItem, {backgroundColor: 'orchid'}]}>
+            <Text>2</Text>
+          </TouchableOpacity>
+        ]}
+      >
+        <View style={[styles.listItem, {backgroundColor: 'salmon'}]}>
+          <Text>Example 1</Text>
+        </View>
+      </Swipeable>
+
       </Card>
     );
   }
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20
+  },
+  listItem: {
+    height: 75,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  leftSwipeItem: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingRight: 20
+  },
+  rightSwipeItem: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+
+});
 
 
 const mapStateToProps = (state) => ({
