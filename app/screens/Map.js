affccc2373b472f3ef8c75979c7100970b7fd833
Sam Wang <sam.zz.wang@gmail.com>
2017-06-11 21:46:16 -0700

    Fix blank restaurant list, add MapView

--- app/screens/Map.js
+++ app/screens/Map.js
@@ -7,37 +7,32 @@ const { height, width } = Dimensions.get('window');
 class MapScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Map Screen</Text>
+      <View style={styles.container}>
+        <MapView
+
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          />
       </View>
     );
   }
 }
-// const MapScreen = () => (
-//   <View style={styles.container}>
-//     <MapView
-//
-//       style={styles.map}
-//       initialRegion={{
-//         latitude: 37.78825,
-//         longitude: -122.4324,
-//         latitudeDelta: 0.0922,
-//         longitudeDelta: 0.0421,
-//       }}
-//       />
-//   </View>
-// );
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     ...StyleSheet.absoluteFillObject,
-//     top: 20
-//   },
-//   map: {
-//     ...StyleSheet.absoluteFillObject,
-//     height: height,
-//     width: width
-//   }
-// });
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    top: 20,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    height: height,
+    width: width
+  }
+});
 
 export default MapScreen;
